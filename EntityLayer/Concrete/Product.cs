7a0021572f3ba2520cb3afd93ce004209d883d39
Samet Akca <developer2@cevizsoft.com>
2022-07-26 12:37:12 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+  public class Product
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public DateTime Expirationdate { get; set; }
+        public bool IsActive { get; set; }
+
+       
+        public Category Category { get; set; }
+
+
+
+    }
+}
