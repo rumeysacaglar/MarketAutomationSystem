b49dcf45b9fc0ea41840238e142b1a357eeb43c1
Samet Akca <developer2@cevizsoft.com>
2022-07-26 12:41:30 +0300

    Product and category new Prop Added

--- EntityLayer/Concrete/Category.cs
+++ EntityLayer/Concrete/Category.cs
@@ -8,6 +8,9 @@ namespace EntityLayer.Concrete
 {
   public  class Category
     {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool IsActive { get; set; }
 
     }
 }
--- EntityLayer/Concrete/Product.cs
+++ EntityLayer/Concrete/Product.cs
@@ -8,6 +8,7 @@ namespace EntityLayer.Concrete
 {
   public class Product
     {
+        public int ProductID { get; set; }
         public int CategoryID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
